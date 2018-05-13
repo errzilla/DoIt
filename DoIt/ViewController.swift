6413e0dedf09b10272079bf3510d60a878c26281
Erza Carlsson <erzami@icloud.com>
2018-05-13 10:30:15 -0700

    Added page navigation

--- DoIt/ViewController.swift
+++ DoIt/ViewController.swift
@@ -8,18 +8,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tasks : [Task] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tasks = makeTasks()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let task = tasks[indexPath.row]
+        
+        if task.important{
+             cell.textLabel?.text = "‼️ \(task.name)!"
+        }
+        else{
+             cell.textLabel?.text = "  \(task.name)"
+        }
+        
+       
+        
+        return cell
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func makeTasks() -> [Task]{
+        let task0 = Task()
+        task0.name = "Walk the dog"
+        task0.important = false
+        
+        let task1 = Task()
+        task1.name = "Buy Cheese"
+        task1.important = true
+        
+        let task2 = Task()
+        task2.name = "Mow the lawn"
+        task2.important = false
+        
+        return [task0, task1, task2]
+        
+        
     }
-
-
+    @IBAction func plusTapped(_ sender: Any) {
+        performSegue(withIdentifier: "addSegue", sender: nil)
+    }
+    
 }
-
---
+++ Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Erza Carlsson on 5/13/18.
+//  Copyright © 2018 Errzilla. All rights reserved.
+//
+
+import Foundation
+class Task {
+    var name = ""
+    var important = false
+    
+}
